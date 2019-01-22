4a50932f83f31e807041115974204a0fe208a81b
Benwei Zhu <benweizhu@gmail.com>
2019-01-22 10:52:58 +0800

    init

---
+++ src/main/java/me/zeph/gradle/spike/gradletestspike/GradleTestSpikeApplication.java
@@ -0,0 +1,14 @@
+package me.zeph.gradle.spike.gradletestspike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleTestSpikeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradleTestSpikeApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/me/zeph/gradle/spike/gradletestspike/GradleTestSpikeApplicationTests.java
@@ -0,0 +1,17 @@
+package me.zeph.gradle.spike.gradletestspike;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GradleTestSpikeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
